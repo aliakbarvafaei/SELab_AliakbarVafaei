226fa8e45e688f6b8bcd104fc8022b5c571353d7
aliakbarvafaei <aliakbarvafaei.060@gmail.com>
2022-04-07 17:24:11 +0430

    Create main.cpp

---
+++ code/main.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+//  edit file
+//  edit file 2
+
+void swap(int *x,int *y)
+{
+    int *temp=new int;
+    *temp=*x;
+    *x=*y;
+    *y=*temp;
+}
+//  add new branch
+int main()
+{
+   int x=10,y=5;
+
+   swap(&x,&y);
+
+   cout<<x<<" "<<y<<endl;
+
+   return 0;
+}
